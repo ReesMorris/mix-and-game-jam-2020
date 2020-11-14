b7b7b4804dff1bd6f36b0d756a4922e5d1319512
Francesco <sennozeta@gmail.com>
2020-11-14 03:12:11 +0000

    Added buildable area filling

--- Assets/Scripts/BuildableTile/BuildableTile.cs
+++ Assets/Scripts/BuildableTile/BuildableTile.cs
@@ -1,8 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BuildableTile : MonoBehaviour {
 
   public bool empty = true;
+
+  public Sprite[] sprites;
+
+  private SpriteRenderer spriteRenderer;
+  private Color color;
+
+  private void Start() {
+    spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    color = spriteRenderer.material.color;
+    color.a = 0.2f;
+    spriteRenderer.material.color = color;
+  }
+
+  public void UpdateTile() {
+    if (empty) {
+      spriteRenderer.sprite = sprites[0];
+    } else {
+      spriteRenderer.sprite = sprites[1];
+    }
+  }
+
+  void OnMouseOver() {
+    if (empty) {
+      spriteRenderer.sprite = sprites[1];
+    }
+  }
+
+  void OnMouseExit() {
+    if (empty) {
+      spriteRenderer.sprite = sprites[0];
+    }
+  }
+
+
+  private void OnMouseDown() {
+    color.a = 1f;
+    spriteRenderer.material.color = color;
+    empty = false;
+    UpdateTile();
+  }
+
+
 }
