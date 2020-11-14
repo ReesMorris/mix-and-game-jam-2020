4201bcc06352130424cef808e9c119573ae4aebd
Rees <reesmorriscontact@gmail.com>
2020-11-14 15:43:05 +0000

    Add ability to queue chat bubble messages

--- Assets/Scripts/Chat/ChatBubble.cs
+++ Assets/Scripts/Chat/ChatBubble.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChatBubble : MonoBehaviour {
-
   public float textSpeed = 0.05f;
   public RectTransform chatContainer;
+  public GameObject panel;
   public TMPro.TMP_Text displayText;
 
+  private List<ChatMessage> textQueue;
+  private bool queueActive;
+
   void Start() {
-    SetText("Why did I move here? I guess it was the weather.");
+    textQueue = new List<ChatMessage>();
+
+    QueueText(new ChatMessage("Why did I move here? I guess it was the weather...", 0.5f));
   }
 
   void Update() {
@@ -17,22 +22,33 @@ public class ChatBubble : MonoBehaviour {
   }
 
   // Sets the bubble text by commencing the coroutine
-  public void SetText(string message) {
-    ClearText();
-    StartCoroutine(TypeText(message));
-  }
+  public void QueueText(ChatMessage message) {
+    textQueue.Add(message);
+    if (!queueActive) StartCoroutine(TypeText(message));
 
-  // Clears current bubble text, but does not hide the bubble
-  public void ClearText() {
-    displayText.text = "";
   }
 
   // Coroutine for typing animation
-  private IEnumerator TypeText(string message) {
-    for (int i = 0; i < message.Length; i++) {
-      displayText.text += message[i];
+  private IEnumerator TypeText(ChatMessage chatMessage) {
+    displayText.text = "";
+    queueActive = true;
+
+    yield return new WaitForSeconds(chatMessage.delayBefore);
+    panel.SetActive(true);
+
+    for (int i = 0; i < chatMessage.message.Length; i++) {
+      displayText.text += chatMessage.message[i];
       yield return new WaitForSeconds(textSpeed);
     }
+
+    yield return new WaitForSeconds(1f);
+
+    textQueue.RemoveAt(0);
+    panel.SetActive(false);
+
+    if (textQueue.Count > 0) StartCoroutine(TypeText(textQueue[0]));
+    else queueActive = false;
+
   }
 
   // Looks at the camera
---
+++ Assets/Scripts/Chat/ChatMessage.cs
@@ -0,0 +1,15 @@
+﻿[System.Serializable]
+public class ChatMessage {
+
+  public ChatMessage(string _message) {
+    message = _message;
+    delayBefore = 0;
+  }
+  public ChatMessage(string _message, float _delayBefore) {
+    message = _message;
+    delayBefore = _delayBefore;
+  }
+
+  public string message;
+  public float delayBefore;
+}
