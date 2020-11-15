ffb64f39a6b24d94619c4ceae3cfc937876af9ab
Rees <reesmorriscontact@gmail.com>
2020-11-15 18:27:18 +0000

    Add ability to toggle music/sound volumes

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
 
+  public AudioSource music;
+
   private AudioSource audioSource;
   private GameObject audioFolder;
+  private bool muteSFX;
 
   void Start() {
     audioFolder = GameObject.Find("Audio");
   }
 
+  public void MuteMusic(bool muteMusic) {
+    music.volume = muteMusic ? 0 : 1;
+  }
+  public void MuteSFX(bool muteSoundEffects) {
+    muteSFX = muteSoundEffects;
+  }
+
   public void PlaySound(AudioClip clip, bool randomPitch) {
-    if (clip) {
-      GameObject item = Instantiate(new GameObject());
-      AudioSource audioSource = item.AddComponent<AudioSource>();
-      audioSource.clip = clip;
-      if (randomPitch) audioSource.pitch = Random.Range(0.8f, 1.2f);
-      audioSource.Play();
-    } else {
-      Debug.LogWarning("Audio clip not provided");
+    if (!muteSFX) {
+      if (clip) {
+        GameObject item = Instantiate(new GameObject());
+        AudioSource audioSource = item.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        if (randomPitch) audioSource.pitch = Random.Range(0.8f, 1.2f);
+        audioSource.Play();
+      } else {
+        Debug.LogWarning("Audio clip not provided");
+      }
     }
   }
 }
---
+++ Assets/SoundButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundButton : MonoBehaviour {
+
+  public enum Types { Music, Sound };
+  public Types type;
+  public Image iconImage;
+  public Sprite mutedIcon;
+  public Sprite unmutedIcon;
+
+  private AudioManager audioManager;
+  private bool isMuted;
+
+  void Start() {
+    audioManager = GameObject.Find("GameManager").GetComponent<AudioManager>();
+  }
+
+  public void OnClick() {
+    isMuted = !isMuted;
+    if (type == Types.Music) audioManager.MuteMusic(isMuted);
+    if (type == Types.Sound) audioManager.MuteSFX(isMuted);
+
+    iconImage.sprite = isMuted ? mutedIcon : unmutedIcon;
+  }
+}
